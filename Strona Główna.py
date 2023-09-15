88c35cb1b6db69390a346e0e5e8d07cfa06ca687
alblesniak <alblesniak@users.noreply.github.com>
2023-09-15 06:08:35 +0000

    new sites

---
+++ Strona Główna.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Streamlit cofnig
+st.set_page_config(page_title="Strona główna", layout="wide")
+
+# Title
+st.markdown("# Kultury religijne współczesnego polskiego katolicyzmu i pentekostalizmu")
+st.markdown("## Analiza danych tekstowych z wykorzystaniem narzędzi lingwistyki komputerowej")
+st.write('Strona tymczasowa')
